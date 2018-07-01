b1b20f73ff8cb40dfa79412e9fdc50baf729afa4
modolee <lsi1318@gmail.com>
2018-07-01 17:00:04 +0900

    SongController 뼈대 작성 중

--- contracts/song/SongController.sol
+++ contracts/song/SongController.sol
@@ -1,10 +1,43 @@
 pragma solidity ^0.4.24;
 
-import "./SongBase.sol";
+import  "./SongBase.sol";
+import "./SongToken.sol";
 
 contract SongController is SongBase {
+  constructor() {
+  }
 
-    constructor() public {
+  /// @notice 음원 토큰 등록
+  function createToken(
+    string _artist,
+    string _title,
+    string _fileURI,
+    string _capacity
+  )
+    public
+  {
+    SongInfo memory newSong = SongInfo({
+      artist:_artist,
+      title:_title,
+      fileURI:_fileURI,
+      creator:_msg.sender(),
+      time:now,
+      capacity:_capacity
+    });
 
-    }
+    new SongToken(newSong);
+  }
+
+  /// @notice 판매 등록
+  function sell(SongToken _address, uint8 _id) public onlyOwnerOf {
+  }
+
+  /// @notice 판매 취소
+  function cancelSell(SongToken _address, uint8 _id) public onlyOwnerOf {
+
+  }
+
+  /// @notice 구매
+  function buy(SongToken _address, uint8 _id) public payable {
+  }
 }
